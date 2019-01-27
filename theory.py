388f1426e7f7ad131221cb30d20b198ed4e9bf1e
Kishan Nirghin <kishanjay@live.nl>
2019-01-27 21:10:31 +0100

    Adds theory placeholders

--- theory.py
+++
@@ -1,7 +0,0 @@
-print "This is the first example taken from the text book, when having a super super super long line one can cut it off " \
-      "using a slash and continue on the next"
-
-
-'''
-constants do not really exist, but use them as such
-'''
